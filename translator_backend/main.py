c3f72a022e2ecad459fe27e2e2ea26ce385b1dc0
Vishal P <118256717+Vishalvishal-2005@users.noreply.github.com>
2025-08-15 17:48:50 +0530

    Update main.py

--- translator_backend/main.py
+++ translator_backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI(title="OCR + Translate API")
 # Allow CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://image-livetranslator.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
